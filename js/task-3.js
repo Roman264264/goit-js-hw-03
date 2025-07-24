398c38ba115e8a09e09d1baeae6a3c6e8665f349
Roman_Lubennikov <lubennikovroman@gmail.com>
2025-07-24 19:30:13 +0300

    js

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,18 @@
-function checkForSpam(message) {
-const blackList = message.toLowerCase()
-return blackList.includes("spam") || blackList.includes("sale")
+function filterArray(numbers, value) {
+const result = [];
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    if (element > value) {
+        result.push(element);
 
+    }
+    
+}
+return result;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
